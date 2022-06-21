c192afadb4b04635c6b7046b84b3819bd9089e1f
John Molakvoæ <skjnldsv@protonmail.com>
2022-06-21 15:30:12 +0200

    Update examples and remove broken Nuxt
    Signed-off-by: John Molakvoæ <skjnldsv@protonmail.com>

--- examples/vue-cli/src/components/vimeo.vue
+++ examples/vue-cli/src/components/vimeo.vue
@@ -4,7 +4,7 @@
 		<vue-plyr>
 			<div class="plyr__video-embed">
 				<iframe
-					src="https://player.vimeo.com/video/143418951?loop=false&amp;byline=false&amp;portrait=false&amp;title=false&amp;speed=true&amp;transparent=0&amp;gesture=media"
+					src="https://player.vimeo.com/video/147865858?loop=false&amp;byline=false&amp;portrait=false&amp;title=false&amp;speed=true&amp;transparent=0&amp;gesture=media"
 					allowfullscreen
 					allowtransparency
 					allow="autoplay"
--- examples/vue-cli/src/main.js
+++ examples/vue-cli/src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import VuePlyr from '@skjnldsv/vue-plyr'
-import '@skjnldsv/vue-plyr/dist/dist/vue-plyr.css'
+import '@skjnldsv/vue-plyr/dist/vue-plyr.css'
 
 createApp(App).use(VuePlyr).mount('#app')
