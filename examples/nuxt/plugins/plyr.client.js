df4f0750e44b33f66aba78c27bed4f8ab972c13d
John Molakvoæ <skjnldsv@protonmail.com>
2022-08-19 14:57:34 +0200

    Fix lint
    Signed-off-by: John Molakvoæ <skjnldsv@protonmail.com>

--- examples/nuxt/plugins/plyr.client.js
+++ examples/nuxt/plugins/plyr.client.js
@@ -1,9 +1,9 @@
-// eslint-disable-next-line import/no-unresolved,node/no-missing-import
+// eslint-disable-next-line import/no-unresolved,n/no-missing-import
 import { defineNuxtPlugin } from '#app'
 
-// eslint-disable-next-line import/no-unresolved,node/no-missing-import
+// eslint-disable-next-line import/no-unresolved,n/no-missing-import
 import VuePlyr from '@skjnldsv/vue-plyr'
-// eslint-disable-next-line import/no-unresolved,node/no-missing-import
+// eslint-disable-next-line import/no-unresolved,n/no-missing-import
 import '@skjnldsv/vue-plyr/dist/vue-plyr.css'
 
 export default defineNuxtPlugin((nuxtApp) => {
--- examples/vue-cli/src/main.js
+++ examples/vue-cli/src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// eslint-disable-next-line import/no-unresolved,node/no-missing-import
+// eslint-disable-next-line import/no-unresolved,n/no-missing-import
 import VuePlyr from '@skjnldsv/vue-plyr'
-// eslint-disable-next-line import/no-unresolved,node/no-missing-import
+// eslint-disable-next-line import/no-unresolved,n/no-missing-import
 import '@skjnldsv/vue-plyr/dist/vue-plyr.css'
 
 createApp(App).use(VuePlyr).mount('#app')
